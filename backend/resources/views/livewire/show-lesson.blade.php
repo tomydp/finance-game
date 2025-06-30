6f65433d30372b9dbae107b4f515960d65a96a20
Ismael Rivarola <tu-correo@example.com>
2025-06-30 15:28:51 -0300

    cambiamos el diseño de el header de las tablas del backoffice

--- backend/resources/views/livewire/show-lesson.blade.php
+++ backend/resources/views/livewire/show-lesson.blade.php
@@ -3,13 +3,13 @@
         <h2 class="text-2xl font-bold mb-4">Lecciones</h2>
         <div class="overflow-x-auto">
             <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
+                <thead class="bg-gray-300">
                     <tr>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">ID</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">Nombre</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">Descripcion</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">Dificultad</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">Acciones</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">ID</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Nombre</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Descripcion</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Dificultad</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Acciones</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
--- backend/resources/views/livewire/show-users.blade.php
+++ backend/resources/views/livewire/show-users.blade.php
@@ -3,19 +3,19 @@
         <h2 class="text-2xl font-bold mb-4">Usuarios</h2>
         <div class="overflow-x-auto">
             <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
+                <thead class="bg-gray-300">
                     <tr>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">ID</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">Nombre</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">Email</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">Rol</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-gray-500 uppercase tracking-wider leading-normal">Creado</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">ID</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Nombre</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Email</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Rol</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Creado</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
                     @foreach($users as $user)
                         <tr>
-                            <td class="px-6 py-4 whitespace-nowrap">{{ $user->id }}</td>
+                            <td class="px-6 py-7 whitespace-nowrap">{{ $user->id }}</td>
                             <td class="px-6 py-4 whitespace-nowrap">{{ $user->name }}</td>
                             <td class="px-6 py-4 whitespace-nowrap">{{ $user->email }}</td>
                             <td class="px-6 py-4 whitespace-nowrap">{{ $user->role ?? 'Usuario' }}</td>
