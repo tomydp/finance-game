4d160999cf7efa0c7631f9d723adff4905bb26da
Tomy <tomydp96@gmail.com>
2025-07-21 17:08:50 -0300

    Se protegieron rutas

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,31 +6,29 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\LessonController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/dashboard')->middleware('auth');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Course
+    Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
+    Route::get('/courses/{course}/edit', [CourseController::class, 'edit'])->name('courses.edit');
+    Route::put('/courses/{course}', [CourseController::class, 'update'])->name('courses.update');
+
+    //User
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+    //Lesson
+    Route::get('/lessons', [LessonController::class, 'index'])->name('lessons.index');
+    Route::get('/lessons/{lesson}/edit', [LessonController::class, 'edit'])->name('lessons.edit');
+    Route::put('/lessons/{lesson}', [LessonController::class, 'update'])->name('lessons.update');
 });
-
-//User 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-
-//Course 
-Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
-Route::get('/courses/{course}/edit', [CourseController::class, 'edit'])->name('courses.edit');
-Route::put('/courses/{course}', [CourseController::class, 'update'])->name('courses.update');
-
-//Lesson 
-Route::get('/lessons', [LessonController::class, 'index'])->name('lessons.index');
-Route::get('/lessons/{lesson}/edit', [LessonController::class, 'edit'])->name('lessons.edit');
-Route::put('/lessons/{lesson}', [LessonController::class, 'update'])->name('lessons.update');
-
 require __DIR__.'/auth.php';
