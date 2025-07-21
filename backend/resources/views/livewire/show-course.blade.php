1677dcb15c3d1e15d329b764b9a7e64fb6b32fc3
Tomy <tomydp96@gmail.com>
2025-07-21 15:48:15 -0300

    Se acomodo modal de cursos

--- backend/resources/views/livewire/show-course.blade.php
+++ backend/resources/views/livewire/show-course.blade.php
@@ -1,15 +1,14 @@
 <div class="container mx-auto p-4">
     <div class="bg-white rounded-lg shadow p-6">
-        <h2 class="text-2xl font-bold mb-4">Cursos</h2>
         <div class="overflow-x-auto">
             <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-300">
+                <thead class="bg-blue-500">
                     <tr>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">ID</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Nombre</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Descripcion</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Dificultad</th>
-                        <th class="px-6 py-7 text-left text-sm font-medium text-black-500 uppercase tracking-wider leading-normal">Acciones</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-white uppercase tracking-wider leading-normal">ID</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-white uppercase tracking-wider leading-normal">Nombre</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-white uppercase tracking-wider leading-normal">Descripcion</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-white uppercase tracking-wider leading-normal">Dificultad</th>
+                        <th class="px-6 py-7 text-left text-sm font-medium text-white uppercase tracking-wider leading-normal">Acciones</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
@@ -22,7 +21,7 @@
                             <td class="px-6 py-7 whitespace-nowrap">{{ $course->difficulty }}</td>
                             <td class="px-6 py-7 whitespace-nowrap">
                                 <button wire:click="editCourse({{ $course->id }})"
-                                        class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
+                                        class="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition">
                                     Editar
                                 </button>
                             </td>
