5fca7d584e2e331ccbba39cd5097413d9068b6fb
Ismael Rivarola <tu-correo@example.com>
2025-08-27 19:13:51 -0300

    Se agrego paginacion en las tablas, como tambien se copio el diseño del modulo de cursos y se hizo igual en los otros modulos

--- backend/app/Livewire/ShowUsers.php
+++ backend/app/Livewire/ShowUsers.php
@@ -3,19 +3,19 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use App\Models\User;
 
 class ShowUsers extends Component
 {
-    public $users;
-    
-    public function mount()
-    {
-        $this->users = User::all();
-    }
-    
+    use WithPagination;
+
+    protected string $paginationTheme = 'tailwind';
+
     public function render()
     {
-        return view('livewire.show-users');
+        return view('livewire.show-users', [
+            'users' => User::orderBy('id')->paginate(10),
+        ]);
     }
 }
