3a90fbb125993e873cec7d53a72706c4128c0c5a
Tomy <tomydp96@gmail.com>
2025-09-03 17:51:22 -0300

    Se acomodo las rutas con rol de admin

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,7 +13,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'role:admin'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
